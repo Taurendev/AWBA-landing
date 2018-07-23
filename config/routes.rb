a8946e45665544a0830371d0e6f3b35241ca9c2a
Shawn Wilson <acounting@taurenltd.com>
2018-07-23 17:27:38 -0600

    Adds Home Controller and Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-
   root 'landing#index'
+
+  get '/index' => 'home#index', :as => :index
+  get '/about' => 'home#about', :as => :about
+  get '/contact' => 'home#contact', :as => :contact
+  get '/board' => 'home#board', :as => :board
 end
